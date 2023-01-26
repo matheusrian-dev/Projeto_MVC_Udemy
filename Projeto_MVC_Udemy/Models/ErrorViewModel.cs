ab3c0a50404d21231631ed6b50ca87a699fdf29f
Matheus Rian <matheusrian332@hotmail.com>
2023-01-26 14:04:19 -0300

    Adicionar arquivos de projeto.

---
+++ Projeto_MVC_Udemy/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Projeto_MVC_Udemy.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
