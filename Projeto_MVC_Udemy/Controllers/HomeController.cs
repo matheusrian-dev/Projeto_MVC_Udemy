d614efbd27490ccbd55eb4c7930267fc1f1f1ad3
Matheus Rian <matheusrian332@hotmail.com>
2023-01-27 14:07:09 -0300

    Inserindo View Demo

--- Projeto_MVC_Udemy/Controllers/HomeController.cs
+++ Projeto_MVC_Udemy/Controllers/HomeController.cs
@@ -18,6 +18,11 @@ namespace Projeto_MVC_Udemy.Controllers
             return View();
         }
 
+        public IActionResult Demo()
+        {
+            return View();
+        }
+
         public IActionResult Privacy()
         {
             return View();
