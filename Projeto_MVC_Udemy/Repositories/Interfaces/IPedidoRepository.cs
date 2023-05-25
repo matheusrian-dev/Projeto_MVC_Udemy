226d02efe3363f78daf8016fd02030d5f113c6d3
Matheus Rian <matheusrian332@hotmail.com>
2023-05-25 14:21:35 -0300

    Criando a Interface IPedidoRepository

---
+++ Projeto_MVC_Udemy/Repositories/Interfaces/IPedidoRepository.cs
@@ -0,0 +1,9 @@
+﻿using Projeto_LanchesMac_Udemy.Models;
+
+namespace Projeto_LanchesMac_Udemy.Repositories.Interfaces
+{
+    public interface IPedidoRepository
+    {
+        void CriarPedido(Pedido pedido);
+    }
+}
