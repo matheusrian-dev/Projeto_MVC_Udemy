ae90d57bf8891e6151bebd934e509a385e0fcc9d
Matheus Rian <matheusrian332@hotmail.com>
2023-05-29 14:13:02 -0300

    Implementação do Controller - PedidoController

---
+++ Projeto_MVC_Udemy/Controllers/PedidoController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Projeto_LanchesMac_Udemy.Models;
+using Projeto_LanchesMac_Udemy.Repositories.Interfaces;
+
+namespace Projeto_LanchesMac_Udemy.Controllers
+{
+    public class PedidoController : Controller
+    {
+        private readonly IPedidoRepository _pedidoRepository;
+        private readonly CarrinhoCompra _carrinhoCompra;
+
+        public PedidoController(IPedidoRepository pedidoRepository, CarrinhoCompra carrinhoCompra)
+        {
+            _pedidoRepository = pedidoRepository;
+            _carrinhoCompra = carrinhoCompra;
+        }
+
+        [HttpGet]
+        public IActionResult Checkout()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Checkout(Pedido pedido)
+        {
+            return View();
+        }
+    }
+}
