35c9dc71cf7d754deef260e064342777d5188d18
Matheus Rian <matheusrian332@hotmail.com>
2023-05-30 14:17:02 -0300

    Implementação da View Checkout e da Action Checkout(POST)

--- Projeto_MVC_Udemy/Controllers/PedidoController.cs
+++ Projeto_MVC_Udemy/Controllers/PedidoController.cs
@@ -24,7 +24,46 @@ namespace Projeto_LanchesMac_Udemy.Controllers
         [HttpPost]
         public IActionResult Checkout(Pedido pedido)
         {
-            return View();
+            int totalItensPedido = 0;
+            decimal precoTotalPedido = 0.0m;
+
+            //Obtém os itens do carrinho de compra do cliente
+            List<ItemCarrinhoCompra> items = _carrinhoCompra.GetCarrinhoCompraItens();
+
+            //verifica se existem itens de pedido
+            if(_carrinhoCompra.ItensCarrinhoCompra.Count == 0)
+            {
+                ModelState.AddModelError("", "Seu carrinho está vazio, que tal incluir um lanche?");
+            }
+            
+            //calcula o total de itens e o total do pedido
+            foreach (var item in items)
+            {
+                totalItensPedido += item.Quantidade;
+                precoTotalPedido += (item.Lanche.Preco * item.Quantidade);
+            }
+
+            //atribui os valores obtidos ao pedido
+            pedido.TotalItensPedido = totalItensPedido;
+            pedido.PedidoTotal = precoTotalPedido;
+
+            //valida os dados do pedido
+            if (ModelState.IsValid)
+            {
+                //cria o pedido e os detalhes
+                _pedidoRepository.CriarPedido(pedido);
+
+                //define mensagens ao cliente
+                ViewBag.CheckoutCompleteMensagem = "Obrigado pelo seu pedido! :D";
+                ViewBag.TotalPedido = _carrinhoCompra.GetCarrinhoCompraTotal();
+
+                //limpa o carrinho do cliente
+                _carrinhoCompra.LimparCarrinho();
+
+                //exibe a view com dados do cliente e do pedido
+                return View("~/Views/Pedido/CheckoutCompleto.cshtml", pedido);
+            }
+            return View(pedido);
         }
     }
 }
