07492ff3e9dc9335c1a835bfcb69703d53d6b920
Matheus Rian <matheusrian332@hotmail.com>
2023-06-05 14:36:09 -0300

    Implementação do Login

---
+++ Projeto_MVC_Udemy/ViewModels/LoginViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Projeto_LanchesMac_Udemy.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "Informe o nome")]
+        [Display(Name ="Usuário")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage ="Informe a senha")]
+        [Display(Name ="Senha")]
+        public string Password { get; set; }
+        public string ReturnUrl { get; set; }
+    }
+}
