53012dfebac8b4ad666398ea78d500d4852f14e4
Matheus Rian <matheusrian332@hotmail.com>
2023-06-28 15:00:11 -0300

    Criação da LoginViewModel

--- Projeto_MVC_Udemy/ViewModels/LoginViewModel.cs
+++ Projeto_MVC_Udemy/ViewModels/LoginViewModel.cs
@@ -9,6 +9,7 @@ namespace Projeto_LanchesMac_Udemy.ViewModels
         public string UserName { get; set; }
 
         [Required(ErrorMessage ="Informe a senha")]
+        [DataType(DataType.Password)]
         [Display(Name ="Senha")]
         public string Password { get; set; }
         public string ReturnUrl { get; set; }
